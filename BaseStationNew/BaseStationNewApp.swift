18fecb92af78944985d2104376e88dd8cff39530
Ruslan <ruslan.2130duda@gmail.com>
2021-04-02 09:50:42 +0300

    Initial Commit

---
+++ BaseStationNew/BaseStationNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseStationNewApp.swift
+//  BaseStationNew
+//
+//  Created by Ruslan Duda on 02.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct BaseStationNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaseStationNew/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BaseStationNew
+//
+//  Created by Ruslan Duda on 02.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
